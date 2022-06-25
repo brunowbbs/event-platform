b4d100524999be42ea806c7bc9bd823587e39187
Wesley Bruno <engwesleybruno@gmail.com>
2022-06-25 15:24:44 -0300

    chore: sidebar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
+import { Event } from "./pages/Event";
 
-interface Lesson {
-  id: string;
-  title: string;
+export default function App() {
+  return <Event />;
 }
-
-function App() {
-  const GET_LESSONS_QUERY = gql`
-    query {
-      lessons {
-        id
-        title
-      }
-    }
-  `;
-
-  /*useEffect(() => {
-    client.query({ query: GET_LESSONS_QUERY }).then((response) => {
-      console.log(response);
-    });
-  }, []);*/
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
-}
-
-export default App;
---
+++ src/App_temp.tsx
@@ -0,0 +1,35 @@
+import { gql, useQuery } from "@apollo/client";
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  const GET_LESSONS_QUERY = gql`
+    query {
+      lessons {
+        id
+        title
+      }
+    }
+  `;
+
+  /*useEffect(() => {
+    client.query({ query: GET_LESSONS_QUERY }).then((response) => {
+      console.log(response);
+    });
+  }, []);*/
+
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <ul>
+      {data?.lessons.map((lesson: Lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default App;
